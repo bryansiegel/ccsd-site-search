fb4ebf3b834594b293af966ef474e83dcce7f7ae
Bryan Siegel <bryansiegel@gmail.com>
2025-10-28 13:56:53 -0700

    major refactor

--- add_unique_constraint.sql
+++
@@ -1,6 +0,0 @@
--- Add unique constraint to prevent duplicate URLs per website
--- This should be run after cleaning up existing duplicates
-
-ALTER TABLE scraped_pages 
-ADD CONSTRAINT unique_website_url 
-UNIQUE (website_id, url(191));
